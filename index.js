a372a7aa4152d5727ddccdbf4470bca74f4c1de9
Tony Eder <tonyeder11@gmail.com>
2023-10-23 14:24:38 -0700

    good to go

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const input = document.getElementById('button');
+// const input = document.getElementById('button');
 
-function addingEventListener() {
-input.addEventListener('click', function() {
-  alert('I was clicked!');
-});
-}
+// function addingEventListener() {
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+// }
 
-addingEventListener();
+// addingEventListener();
+ 
+
+
+ function addingEventListener(){
+  const input = document.getElementById('button').addEventListener('click', function(){
+    alert("I have been clicked!")
+  })
+ }
+ 
